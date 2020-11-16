7d468ea8b48763503c57f89105f94e631aabfef9
ARC <arc@arc-dev.resnet.purdue.edu>
2020-11-16 17:36:28 -0600

    IK + Planning works full stack

--- arc_robot_arm/moveit_arduino/moveit_arduino/moveit_arduino_4dof/moveit_arduino_4dof.ino
+++ arc_robot_arm/moveit_arduino/moveit_arduino/moveit_arduino_4dof/moveit_arduino_4dof.ino
@@ -23,48 +23,70 @@
 #include <std_msgs/UInt16.h>
 #include <sensor_msgs/JointState.h>
 
-#define Rev1_PIN 11
-#define Rev2_PIN 10
-#define Rev3_PIN 9
-#define Rev4_PIN 6
-#define Rev5_PIN 5
-#define Rev6_PIN 4
+#define Rev1_PIN 3
+#define Rev2_PIN 5
+#define Rev3_PIN 6
+#define Rev4_PIN 9
+#define Rev5_PIN 10
+#define Rev6_PIN 11
+
 #define DOF 5
+#define OFFSET 90
 
 ros::NodeHandle  nh;
 
-Servo rev1;
-Servo rev2;
-Servo rev3;
-Servo rev4;
-Servo rev5;
-Servo rev6;
+Servo rev1; // base servo
+Servo rev2; // shoulder servo
+Servo rev3; // elbow servo
+Servo rev4; // wrist roll servo
+Servo rev5; // wrist pitch servo
+Servo rev6; // gripper servo
 
-char print_str[15];
+char print_str[15]; // used to print out 15 char outputs
 
-float gripper;
-float arm[DOF];
+float gripper = 0.0; // angle in degrees [0-45]
+float arm[DOF] = {90,90,90,90,90}; // angles in degrees [0,180]
 
 inline float RadiansToDegrees(float position_radians)
 {
   return position_radians * 57.2958;
 }
 
-void set_gripper_joints(const sensor_msgs::JointState& joint_state){
-  gripper = RadiansToDegrees(joint_state.position[1]); // left finger actuation only
+int val_arm_joints() {
+  for(int i = 0; i < DOF; i++){
+    if(arm[i] < 0 || arm[i] > 180){
+      return 0;
+    }
+  }
+  return 1;
 }
 
+int val_gripper_joint() {
+  return gripper >= 0 && gripper <= 45;
+}
+
+// Sets gripper joint from JointState msg
+void set_gripper_joint(const sensor_msgs::JointState& joint_state){
+  gripper = abs(RadiansToDegrees(joint_state.position[1])); // left finger actuation only
+}
+
+/* Sets arm joint values from JointState msg 
+*  and maps them to real arm space
+*/
 void set_arm_joints(const sensor_msgs::JointState& joint_state) {
-  arm[0] = RadiansToDegrees(joint_state.position[0]);
-  arm[1] = RadiansToDegrees(joint_state.position[1]);
-  arm[2] = RadiansToDegrees(joint_state.position[2]);
-  //arm[3] = joint_state.position[3];
-  arm[4] = RadiansToDegrees(joint_state.position[3]);
+  arm[0] = RadiansToDegrees(joint_state.position[0]) + OFFSET;
+  arm[1] = RadiansToDegrees(joint_state.position[1]) + OFFSET;
+  arm[2] = RadiansToDegrees(joint_state.position[2]) + OFFSET;
+  //arm[3] = joint_state.position[3] + OFFSET;
+  arm[4] = RadiansToDegrees(joint_state.position[3]) * -1 + OFFSET;
 }
 
+// checks whether the JointState msg is for the arm or gripper movegroup
 int is_arm_move_group(const sensor_msgs::JointState& joint_state){
+  // Name of first joint (either Rev1 or Rev5)
   String first_joint = joint_state.name[0]; 
-  String arm_first_joint = "Rev1";
+  String arm_first_joint = "Rev1"; // first joint of arm
+
   nh.loginfo(joint_state.name[0]);
   return first_joint == arm_first_joint;
 }
@@ -81,25 +103,52 @@ void print_gripper(){
   nh.loginfo(print_str);
 }
 
-void joint_control_cb( const sensor_msgs::JointState& joint_state) { // output can be either for gripper joint or for arm joints
+void drive_arm(){
+  if(val_arm_joints()){
+    rev1.write(arm[0]);
+    rev2.write(arm[1]);
+    rev3.write(arm[2]);
+    //rev4.write(arm[3]);
+    rev5.write(arm[4]);
+  }
+  else{
+    nh.loginfo("Invalid Arm Joint Value!");
+  }
+}
+
+void drive_gripper(){
+  if(val_gripper_joint()){
+    rev6.write(gripper);
+  }
+  else{
+    nh.loginfo("Invalid Gripper Joint Value!");
+  }
+}
+
+/* Callback for JointState subscriber, runs when new JointState
+*  is published to move_group/fake_controller_joint_states
+*/
+void joint_control_cb(const sensor_msgs::JointState& joint_state) { // output can be either for gripper joint or for arm joints
   if(is_arm_move_group(joint_state))  {
     set_arm_joints(joint_state);
     nh.loginfo("arm");
     print_arm();
+    drive_arm();    
   }
   else{
-    set_gripper_joints(joint_state);
+    set_gripper_joint(joint_state);
     nh.loginfo("gripper");
     print_gripper();
+    drive_gripper();
   }
 }
 
-ros::Subscriber<sensor_msgs::JointState> sub("move_group/fake_controller_joint_states", joint_control_cb);
+ros::Subscriber<sensor_msgs::JointState> joints_sub("move_group/fake_controller_joint_states", joint_control_cb);
 
 void setup(){
   Serial.begin(4800);
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(joints_sub);
   
   rev1.attach(Rev1_PIN);
   rev2.attach(Rev2_PIN);
@@ -107,16 +156,16 @@ void setup(){
   rev4.attach(Rev4_PIN);
   rev5.attach(Rev5_PIN);
   rev6.attach(Rev6_PIN);
-  rev1.write(90);
-  rev2.write(90);
-  rev3.write(90);
-  rev4.write(90);
-  rev5.write(90);
-  rev6.write(90);
-
+  
+  rev1.write(arm[0]);
+  rev2.write(arm[1]);
+  rev3.write(arm[2]);
+  rev4.write(arm[3]);
+  rev5.write(arm[4]);
+  rev6.write(gripper);
 }
 
 void loop(){
-  //nh.spinOnce();
+  nh.spinOnce();
   delay(1);
 }
