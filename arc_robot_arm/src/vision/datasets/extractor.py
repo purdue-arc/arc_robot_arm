9b932fe0d329d7acde00d7394dc233ad8de35e32
Raghava <raghava.upp13@gmail.com>
2021-01-03 11:30:18 -0600

    README changes

--- arc_robot_arm/src/vision/datasets/extractor.py
+++ arc_robot_arm/src/vision/datasets/extractor.py
@@ -3,12 +3,15 @@ import os
 print("Enter folder name to extract file name types from: ")
 folder_name = input()
 
+print("Enter file to append extraction: ")
+file_name = input()
+
 mypath = os.path.dirname(os.path.abspath(__file__)) + "/" + folder_name
 data = os.listdir(mypath)
 
 data = set(map(lambda x: "_".join(x.split("_")[:-1]),data))
 data = list(filter(lambda x: x[0] != '.' ,data))
 
-with open("rules.txt", "a") as f:
+with open(file_name, "a") as f:
     for name in data:
-        f.write("%s\n" % name)
+        f.write(" - ![%(name)s](%(dir_n)s/%(name)s_0.jpg)\n" % {"name" : name, "dir_n" : folder_name})
