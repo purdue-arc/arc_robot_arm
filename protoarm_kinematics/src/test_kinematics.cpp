3da9b417e3b2c1eb2de894422dca893b0b46a6c6
Raghava Uppuluri <raghavauppuluri13@gmail.com>
2022-01-26 18:14:00 -0500

    fixed bug with planning start state

--- protoarm_kinematics/src/test_kinematics.cpp
+++ protoarm_kinematics/src/test_kinematics.cpp
@@ -17,30 +17,30 @@ std::vector<geometry_msgs::Pose> testFileToPoses(std::string test_file_name) {
 	std::string line;
 
 	while (std::getline(infile, line)) {
-			if(line.at(0) != '#') {  // NOTE: Comment lines in test txt files start with '#'
-				std::istringstream iss(line);
-				double x, y, z;
-				if (!(iss >> x >> y >> z)) { break; } // error
+		if(line.at(0) != '#') {  // NOTE: Comment lines in test txt files start with '#'
+			std::istringstream iss(line);
+			double x, y, z;
+			if (!(iss >> x >> y >> z)) { break; } // error
 
-				ROS_INFO_STREAM(x << " " << y << " " << z << "\n");
+			ROS_INFO_STREAM(x << " " << y << " " << z << "\n");
 
-				geometry_msgs::Pose pose;
-				pose.position.x = x;
-				pose.position.y = y;
-				pose.position.z = z;
-				test_goal_poses.push_back(pose);	
-			}
-	}
+			geometry_msgs::Pose pose;
+			pose.position.x = x;
+			pose.position.y = y;
+			pose.position.z = z;
+			test_goal_poses.push_back(pose);	
+		}
+}
 
-	return test_goal_poses;
+return test_goal_poses;
 }
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "test_kinematics");
-  ros::NodeHandle node_handle("~");
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-  ROS_INFO("Waiting for context to come up");
+	ros::init(argc, argv, "test_kinematics");
+	ros::NodeHandle node_handle("~");
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+	ROS_INFO("Waiting for context to come up");
 
 	Kinematics robot_arm = Kinematics();
 	std::vector<geometry_msgs::Pose> test_goal_poses = testFileToPoses("test_kinematics.txt");
